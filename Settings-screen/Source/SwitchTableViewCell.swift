135e412d061814b173ffd7f834f6545dafb064c4
Yzee <yan.zhigurs@mail.ru>
2023-01-06 19:50:03 +0500

    add layout and add in subview

--- Settings-screen/Source/SwitchTableViewCell.swift
+++ Settings-screen/Source/SwitchTableViewCell.swift
@@ -39,5 +39,40 @@ class SwitchTableViewCell: UITableViewCell {
         mySwitch.translatesAutoresizingMaskIntoConstraints = false
         return mySwitch
     }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
 
+        contentView.addSubview(label)
+        contentView.addSubview(iconContainer)
+        contentView.addSubview(mySwitch)
+        iconContainer.addSubview(iconImageView)
+        
+        contentView.clipsToBounds = true
+        accessoryType = .none
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let size: CGFloat = contentView.frame.size.height - 12
+        iconContainer.frame = CGRect(x: 15, y: 6, width: size, height: size)
+        
+        let imageSize: CGFloat = size/1.5
+        iconImageView.frame = CGRect(x: (size-imageSize)/2, y: (size-imageSize)/2, width: imageSize, height: imageSize)
+        
+        mySwitch.sizeToFit()
+        mySwitch.frame = CGRect(x:(contentView.frame.size.width - mySwitch.frame.size.width) - 20.0,
+                                y: (contentView.frame.size.height - mySwitch.frame.size.height) / 2,
+                                width: mySwitch.frame.size.width,
+                                height: mySwitch.frame.size.height)
+        
+        label.frame = CGRect(x: 25 + iconContainer.frame.size.width,
+                             y: 0,
+                             width: contentView.frame.size.width - 20 - iconContainer.frame.size.width,
+                             height: contentView.frame.size.height)
+    }
 }
