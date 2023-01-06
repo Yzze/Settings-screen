0b60462cec8482ddbb38f967b75f603566c2d102
Yzee <yan.zhigurs@mail.ru>
2023-01-06 19:40:58 +0500

    add layout and add in subview

--- Settings-screen/Source/SettingsTableViewCell.swift
+++ Settings-screen/Source/SettingsTableViewCell.swift
@@ -32,5 +32,37 @@ class SettingsTableViewCell: UITableViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(label)
+        contentView.addSubview(iconContainer)
+        iconContainer.addSubview(iconImageView)
+        
+        contentView.clipsToBounds = true
+        accessoryType = .disclosureIndicator
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let size: CGFloat = contentView.frame.size.height - 12
+        iconContainer.frame = CGRect(x: 15, y: 6, width: size, height: size)
+        
+        let imageSize: CGFloat = size/1.5
+        iconImageView.frame = CGRect(x: (size-imageSize)/2,
+                                     y: (size-imageSize)/2,
+                                     width: imageSize,
+                                     height: imageSize)
+        
+        label.frame = CGRect(x: 25 + iconContainer.frame.size.width,
+                             y: 0,
+                             width: contentView.frame.size.width - 20 - iconContainer.frame.size.width,
+                             height: contentView.frame.size.height)
+    }
 
 }
