5365291b73a4984edd7417da41c01a8972c3ae61
Yzee <yan.zhigurs@mail.ru>
2023-01-06 19:26:02 +0500

    creating files for each cell

---
+++ Settings-screen/Source/BadgeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BadgeTableViewCell.swift
+//  Settings-screen
+//
+//  Created by Ян Жигурс on 06.01.2023.
+//
+
+import UIKit
+
+class BadgeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
